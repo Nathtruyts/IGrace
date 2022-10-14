02fbfe343d0e027bff25ff99b9cdda08ebe027ac
Nathan Truyts <nathan.truyts@fatec.sp.gov.br>
2022-10-14 15:41:14 -0300

    firebase finished

--- App.js
+++ App.js
@@ -1,17 +1,21 @@
+
 import React from 'react';
 import { StyleSheet, View} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack'
 import Routes from './src/routes';
+import Details from './src/Stack Pages/Details/index';
+
+const 
 
 export default App = () => {
   
   return (
     <NavigationContainer>
-       <Routes/>
+      <Routes/>
     </NavigationContainer>
-  );
-};
-
+);
+}
 const styles = StyleSheet.create({
   container: {
     flex:1,
